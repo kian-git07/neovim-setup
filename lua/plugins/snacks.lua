b47a9d212d8ef7494585980b3e1c88f47f6b62b9
Laptop <davoudi.kian2007@gmail.com>
2026-03-02 17:26:25 -0500

    Initial Commit

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false },
+    },
+  },
+  {
+    "echasnovski/mini.files",
+    lazy = false,
+    opts = {
+      options = {
+        use_as_default_explorer = true,
+      },
+    },
+  },
+}
