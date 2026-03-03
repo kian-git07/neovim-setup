67f56305b921e68b0d450973e0ae60c13455b95d
Laptop <davoudi.kian2007@gmail.com>
2026-03-03 10:33:41 -0500

    Added more colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,27 @@
+
 return {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     { "rose-pine/neovim", name = "rose-pine", priority = 1000 },
     { "sainnhe/gruvbox-material", name = "gruvbox-material", priority = 1000 },
     { "Mofiqul/dracula.nvim", name = "dracula", priority = 1000 },
-
-    -- Configure LazyVim to load gruvbox
+    -- This can be commented out
     {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
-        },
-    },
+        "navarasu/onedark.nvim",
+        version = "v0.1.0", -- Pin to legacy version
+        priority = 1000,
+        config = function()
+            require('onedark').setup {
+            style = 'darker'
+        }
+            require('onedark').load()
+        end
+    -- This is where it stops
+    }
+        -- Configure LazyVim to load gruvbox
+    -- {
+    --     "LazyVim/LazyVim",
+    --     opts = {
+    --         colorscheme = "rose-pine",
+    --     },
+    -- },
 }
