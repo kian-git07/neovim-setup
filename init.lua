1952dbe4587b4ce99e0d92f0f5848b4c75c032bb
PC <davoudi.kian2007@gmail.com>
2026-03-03 08:33:30 -0500

    Added new color schemes

--- init.lua
+++ init.lua
@@ -1,1 +1,4 @@
 require("config.lazy")
+require("lspconfig").clangd.setup({
+    cmd = { "clangd", "--query-deriver=/davou.bin/g++", "--fallback-flag=-std=c++17" },
+})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
-  -- add gruvbox
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "rose-pine/neovim", name = "rose-pine", priority = 1000 },
+    { "sainnhe/gruvbox-material", name = "gruvbox-material", priority = 1000 },
+    { "Mofiqul/dracula.nvim", name = "dracula", priority = 1000 },
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
+    -- Configure LazyVim to load gruvbox
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin",
+        },
     },
-  }
 }
